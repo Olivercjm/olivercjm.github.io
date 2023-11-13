df662c16dfaedb78d2b8e8521196e3eb554d8392
Oliver Chan <138875914+Olivercjm@users.noreply.github.com>
2023-11-13 22:17:03 +0800

    Add files via upload

---
+++ lesson_activity/LIKE.js
@@ -0,0 +1,28 @@
+function news() {
+                document.getElementById("news").scrollIntoView({ behavior: 'smooth' });
+            }
+
+            function men() {
+                document.getElementById("mens").scrollIntoView({ behavior: 'smooth' });
+            }
+
+            function woman() {
+                document.getElementById("womans").scrollIntoView({ behavior: 'smooth' });
+            }
+
+            function kid() {
+                document.getElementById("kids").scrollIntoView({ behavior: 'smooth' });
+            }
+
+            function sale() {
+                document.getElementById("sales").scrollIntoView({ behavior: 'smooth' });
+            }
+
+            function btt() {
+                document.getElementById("btts").scrollIntoView({ behavior: 'smooth' });
+            }
+
+            function aj1()
+            {
+                window.location.href = 'aj1.html';
+            }
